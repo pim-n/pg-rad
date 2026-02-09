d6d9fa6f92ff6d08bfb5ec1992f32bf001f21fcf
Pim Nelissen <pim.nelissen@protonmail.com>
2026-02-09 15:32:49 +0100

    update init for isotopes module

--- src/pg_rad/isotopes/__init__.py
+++ src/pg_rad/isotopes/__init__.py
@@ -2,7 +2,9 @@
 __ignore__ = ["logger"]
 
 from pg_rad.isotopes import isotope
+from pg_rad.isotopes import presets
 
 from pg_rad.isotopes.isotope import (Isotope,)
+from pg_rad.isotopes.presets import (CS137,)
 
-__all__ = ['Isotope', 'isotope']
+__all__ = ['CS137', 'Isotope', 'isotope', 'presets']
