abc1195c917bbcd437151704762383cc43d05b7c
Pim Nelissen <pim.nelissen@protonmail.com>
2026-02-12 14:45:24 +0100

    Rewrite preset isotope

--- src/pg_rad/isotopes/presets.py
+++ src/pg_rad/isotopes/presets.py
@@ -1,4 +1,10 @@
 from .isotope import Isotope
 
 
-CS137 = Isotope("Cs-137", E=661.66, b=0.851)
+class CS137(Isotope):
+    def __init__(self):
+        super.__init__(
+            name="Cs-137",
+            E=661.66,
+            b=0.851
+        )
