ae0a038948e5b57525c5087a17937323a895ce5a
Pim Nelissen <pim.nelissen@protonmail.com>
2026-02-25 14:21:59 +0100

    add SimulationEngine and SimulationOutputs to compute results and pass standardized objects on to plotter

---
+++ src/pg_rad/simulator/outputs.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple
+
+from dataclasses import dataclass
+
+
+@dataclass
+class CountRateOutput:
+    arc_length: List[float]
+    count_rate: List[float]
+
+
+@dataclass
+class SourceOutput:
+    name: str
+    isotope: str
+    activity: float
+    position: Tuple[float, float, float]
+    dist_from_path: float
+
+
+@dataclass
+class SimulationOutput:
+    name: str
+    count_rate: CountRateOutput
+    sources: List[SourceOutput]
