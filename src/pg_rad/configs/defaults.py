58de830a39669460db46178df58b6891c2f68477
Pim Nelissen <pim.nelissen@protonmail.com>
2026-02-25 14:22:46 +0100

    add default values to central location

---
+++ src/pg_rad/configs/defaults.py
@@ -0,0 +1,10 @@
+# --- Physics defaults ---
+DEFAULT_AIR_DENSITY = 1.243  # kg/m^3, Skåne
+
+# --- Simulation defaults ---
+DEFAULT_SEED = None
+DEFAULT_ACQUISITION_TIME = 1.0
+
+# --- Source defaults ---
+DEFAULT_PATH_HEIGHT = 0.0
+DEFAULT_SOURCE_HEIGHT = 0.0
